c3346627a8a888c3181ce1bd35ac68a41ebfda1c
luciouskami <realheiluo@gmail.com>
2019-07-16 02:06:09 +0800

    update dz_w3_plugin

--- Development/Plugin/Warcraft3/yd_dzclient_api/dll_module.cpp
+++ Development/Plugin/Warcraft3/yd_dzclient_api/dll_module.cpp
@@ -2,20 +2,22 @@
 #include "dll_module.h"
 #include "dz_w3_plugin.h"
 #include <fstream>
+#include <warcraft3/jass/func_value.h>
+#include "warcraft3/war3_searcher.h"
+#include "warcraft3/hashtable.h"
 
 
 extern unsigned char dz_w3_plugin_shellcode[];
 
 dll_module::dll_module()
-	:dz_w3_plugin_handle(0)
+	: dz_w3_plugin_handle(0)
 	, game_module_handle(0)
 	, storm_module_handle(0)
 {}
 
 
 dll_module::~dll_module()
-{
-}
+= default;
 
 void dll_module::attach()
 {
@@ -50,5 +52,39 @@ auto dll_module::export_dz_w3_plugin() -> bool
 		return false;
 	}
 	return false;
+}
+
+//.text:1002B853                 mov[ebp + var_1D8], offset sub_10029CF0
+//.text:1002B85D                 mov[ebp + var_1D4], offset aDzapiMapMissio; "DzAPI_Map_MissionComplete"
+//.text:1002B867                 mov[ebp + var_1D0], offset aHplayerSsV; "(Hplayer;SS)V"
+//......
+//.text:1002BC73                 mov[ebp + var_10], offset sub_1002B700
+//.text:1002BC7A                 mov[ebp + var_C], offset aRequestextrare; "RequestExtraRealData"
+//.text:1002BC81                 mov[ebp + var_8], offset aIhplayerSsbiii_2; "(IHplayer;SSBIII)R"
+void dll_module::search_dz_api_from_vm()
+{
+	using warcraft3::hashtable::native_func_node;
+	using warcraft3::get_war3_searcher;
+	using warcraft3::get_native_function_hashtable;
+
+	if (!get_war3_searcher().get_instance(5))
+		return false;
+
+	if (!old_proc_ptr)
+		return false;
+
+	native_func_node* node_ptr =get_native_function_hashtable()->find(proc_name);
+
+	if (!node_ptr)
+		return false;
+
+	*old_proc_ptr = (uintptr_t)node_ptr->func_address_;
+}
+
+void dll_module::add_dzapi_to_lua_engine()
+{
+	//借一个引用过来
+	auto& tmp_ref = warcraft3::jass::japi_function;
+	
 }
 
--- Development/Plugin/Warcraft3/yd_dzclient_api/dll_module.h
+++ Development/Plugin/Warcraft3/yd_dzclient_api/dll_module.h
@@ -8,6 +8,8 @@ public:
 	void detach();
 	//ÊÍ·Å²å¼þ
 	bool export_dz_w3_plugin();
+	void search_dz_api_from_vm();
+	void add_dzapi_to_lua_engine();
 public:
 	uintptr_t dz_w3_plugin_handle;
 	uintptr_t game_module_handle;
